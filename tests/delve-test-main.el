97352002d231ff785de299cbb7e616e0a0a5bad2
Jörg Volbers <joerg@joergvolbers.de>
2021-02-17 18:14:31 +0100

    rename test files

--- tests/delve-test-main.el
+++ tests/delve-test-main.el
@@ -31,9 +31,18 @@
 
 ;; Utilities
 
-;; TODO Replace them with the ones from ./test-delve-pp.el
+(defun delve-test-fake-pages (&optional title)
+  "Return a fake page object with no significant data."
+  (delve-make-page  :title  (if title (format "%s" title) "Fake-Title")
+		     :file  "dummyfile.org"
+		     :tags '("tag1" "tag2")
+		     :mtime (current-time)
+		     :atime (current-time)
+		     :backlinks 0
+		     :tolinks 0))
 
-(defmacro test-delve--test-with-temp-buffer (content &rest body)
+;; TODO Replace this one with the ones from ./test-delve-pp.el
+(defmacro delve-test--with-temp-buffer (content &rest body)
   "Evaluate BODY in a temporary buffer with CONTENT."
   (declare (debug t)
            (indent 1))
@@ -41,7 +50,8 @@
      (insert ,content)
      ,@body))
 
-(defun test-delve--face-covers-range-p (start end face)
+;; TODO Replace this one with the ones from ./test-delve-pp.el
+(defun delve-test--face-covers-range-p (start end face)
   "Return true if every face from START to END has FACE."
   (let ((all-faces (mapcar (lambda (i) (get-text-property i 'face)) (number-sequence start end))))
     (seq-every-p (lambda (target-face)
@@ -52,7 +62,7 @@
 
 ;; this is taken from
 ;; https://github.com/clojure-emacs/cider/blob/master/test/cider-font-lock-tests.el
-(defun test-delve--face-exists-in-range-p (start end face)
+(defun delve-test-face-exists-in-range-p (start end face)
   "Return true if FACE exists between START to END."
   (let ((all-faces (mapcar (lambda (i) (get-text-property i 'face)) (number-sequence start end))))
     ;; cl-some returns t now but will change to return a truthy value in the future
@@ -139,15 +149,52 @@
 	(setq s (delve-represent-tags (delve-make-zettel
 				       :tags tags))))
       (it "uses delve-tags-face"
-	(test-delve--test-with-temp-buffer s
-	  (expect (test-delve--face-exists-in-range-p
+	(delve-test--with-temp-buffer s
+	  (expect (delve-test-face-exists-in-range-p
 		   (point-min) (point-max)
 		   'delve-tags-face)
 		  :to-be-truthy)))
       (it "returns nil when no tags are given"
 	(expect (delve-represent-tags (delve-make-zettel))
 		:to-be nil)))))
-      
 
+;; * Collections
+
+(describe "Collections"
+  (describe "delve-view-collection"
+    :var (some-items)
+    (before-all
+      (setq some-items (seq-map #'delve-test-fake-pages
+				'("Zettel1" "Zettel2" "Zettel3"))))
+
+    (it "returns a delve buffer if called with correct arguments"
+      (let ((buf (delve-view-collection some-items (lambda () (list "test"))) "testbuffer")))
+	(expect (type-of buf) :to-be 'buffer)
+	(expect (with-current-buffer buf major-mode)
+		:to-be 'delve-mode)
+	(kill-buffer buf)))
+    (it "uses the return value of the heading function for the list header"
+      (let* ((my-heading '("My Heading"))
+	     (heading-fn (lambda () my-heading))
+	     (buf (delve-view-collection some-items heading-fn "testbuffer"))
+	     (header (with-current-buffer buf
+		       (or lister-local-header-marker
+			   (buffer-substring-no-properties (+ lister-local-header-marker lister-local-left-margin)
+							   (+ lister-local-header-marker
+							      (1- (get-text-property lister-local-header-marker 'nchars)))))))))
+      (expect (list header) :to-equal my-heading)
+      (kill-buffer buf))
+    (it "accepts a value of nil for the header fn"
+      (let ((buf (delve-view-collection some-items nil "testbuffer")))
+	(expect buf :not :to-be nil)
+	(kill-buffer buf)))
+    (it "inserts all items of the collections"
+      (let* ((n 200)
+	     (many-items (seq-map #'delve-test-fake-pages (number-sequence 1 n)))
+	     (buf (delve-view-collection many-items nil "testbuffer")))
+	(expect (length (lister-get-all-data buf)) :to-be n)
+	(kill-buffer buf))))
+
+    
 (provide 'delve-test)
 ;;; delve-test.el ends here
