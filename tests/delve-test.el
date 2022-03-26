482e45baab790b34b72f8a4ad10b0a10f2215447
Jörg Volbers <joerg@joergvolbers.de>
2022-03-26 18:55:11 +0100

    Add file for testing delve.el

---
+++ tests/delve-test.el
@@ -0,0 +1,33 @@
+;;; delve-test.el --- Tests for delve.el             -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022
+
+;; Author:  <joerg@joergvolbers.de>
+;; Keywords: internal
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Test delve.el functions.
+;; VERY incomplete.
+
+;;; Code:
+(require 'buttercup)
+;; (require 'delve-test-db-utils)
+
+
+
+(provide 'delve-test)
+;;; delve-test.el ends here
