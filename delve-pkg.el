b9769e7d750612e98841a156702568eff1f7d099
Jörg Volbers <joerg@joergvolbers.de>
2022-05-13 18:39:01 +0200

    Bump to v 0.9.4

--- delve-pkg.el
+++ delve-pkg.el
@@ -1,4 +1,4 @@
-(define-package "delve" "0.9.3"
+(define-package "delve" "0.9.4"
   "Delve into your org roam zettelkasten."
   '((emacs "27.2")
     ;; Lister tries to avoid dash, but since Org Roam uses it anyway:
