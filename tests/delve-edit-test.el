3793af63dd7484a70b00af4bb3c730c242fb7b94
Jörg Volbers <joerg@joergvolbers.de>
2023-05-15 19:21:17 +0200

    Add test file for delve-edit.el

---
+++ tests/delve-edit-test.el
@@ -0,0 +1,29 @@
+;;; delve-edit-test.el --- Tests for delve-tests.el  -*- lexical-binding: t; -*-
+;; Copyright (C) 2023
+
+;; Author:  <joerg@joergvolbers.de>
+;; Keywords: internal
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Tests for remote editing Org files.
+
+;;; Code:
+
+
+
+(provide 'delve-edit-test)
+;;; delve-edit-test.el ends here
