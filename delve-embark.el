bef925a764f3056641516a8374b7fbd93a28e2df
Jörg Volbers <joerg@joergvolbers.de>
2023-12-05 08:28:47 +0100

    Add some stubs for Embark support

--- delve-embark.el
+++ delve-embark.el
@@ -25,8 +25,74 @@
 ;;; Code:
 
 (require 'embark)
+(require 'delve)
+(require 'delve-query)
 
+;; TODO Es gibt eine org-link keymap (mit id:xxxx); die kann benutzt
+;;werden, um z.B. das Ziel in eine Delve collecetion zu speichern
+;; TODO Keymap funktioniert nicht
 
+;;; Variables
+
+(defvar delve-embark--target-finders
+  (list #'delve-embark--uuid-finder)
+  "List of Delve specific finders for Embark.")
+
+;; TODO Generisch UUID -> Node dann function
+;; (defun delve-embark--call-node-with-uuid
+
+(defun delve-embark--test-embark (uuids)
+  "Do someting with UUIDS."
+  (org-roam-node-open (delve-query-nodes-by-id uuids)))
+
+(defvar delve-embark-node-map
+  "Keymap for Embark actions on Org Roam Nodes.
+The actions must accept a list of UUIDs, which are passed as a
+strings."
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "o") #'delve-embark--test-embark)
+    map))
+
+(defvar delve-embark--keymap-alist
+  '((delve--uuid . delve-embark-node-map))
+  "Alist defining Embark actions for Delve specific types.")
+
+;; * Define Embark Target Finder
+
+;; Nodes are identified via UUID
+
+(defun delve-embark--node-at-point ()
+  "Return the current Org Roam node at point."
+  (cond
+   ((delve--org-roam-buffer-p)
+    (org-roam-node-at-point))
+   ((delve--buffer-p)
+    (delve--zettel-node (delve--current-item-or-error 'delve--zettel)))))
+
+(defun delve-embark--uuid-finder ()
+  "Return the current Org Roam node UUID at point as an Embark target.
+Target type is `delve--uuid'."
+  (when-let* ((node (delve-embark--node-at-point))
+              (uuid (org-roam-node-id node)))
+    (cons 'delve--uuid uuid)))
+
+;;;; Enable and disable Delve/Embark integration
+
+;; TODO also add keymap
+(defun delve-embark--enable ()
+  "Add Delve-specific functions to Embark."
+  (mapc (apply-partially #'add-hook 'embark-target-finders)
+        delve-embark--target-finders)
+  (pcase-dolist (`(,type . ,keymap) delve-embark--keymap-alist)
+    (setf (alist-get type embark-keymap-alist) keymap)))
+
+(defun delve-embark--disable ()
+  "Remove Delve-specific functions from Embark."
+  (mapc (apply-partially #'remove-hook 'embark-target-finders)
+        delve-embark--target-finders)
+  (setq embark-keymap-alist (cl-set-difference embark-keymap-alist
+                                               delve-embark--keymap-alist
+                                               :test #'equal)))
 
 (provide 'delve-embark)
 ;;; delve-embark.el ends here
