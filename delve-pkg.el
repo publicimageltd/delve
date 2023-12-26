b38a6be9429d8635eafd60b65aa5c3c2cb249d36
Jörg Volbers <joerg@joergvolbers.de>
2023-12-26 20:11:01 +0100

    Bump versions of Org Roam and Dash

--- delve-pkg.el
+++ delve-pkg.el
@@ -2,9 +2,9 @@
   "Delve into your Org Roam zettelkasten."
   '((emacs "27.2")
     ;; Lister tries to avoid dash, but since Org Roam uses it anyway:
-    (dash "2.13")
+    (dash "2.20")
     (transient "0.3.7")
-    (org-roam "2.1")
+    (org-roam "2.2.2")
     (lister "0.9.4"))
   :keywords '("org-roam" "hypermedia")
   :homepage "https://github.com/publicimageltd/delve")
